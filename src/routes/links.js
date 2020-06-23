1066f722ef8c8a928bb9c6ad862012e7c52c21a6
FabianCruz-0 <fabiancruz3773@gmail.com>
2020-06-23 16:17:02 -0500

    FRONTEND AGREGAR UN LINK LISTO

--- src/routes/links.js
+++ src/routes/links.js
@@ -7,6 +7,10 @@ const pool = require('../database'); //importacion de la bd.
 router.get('/add', (req,res) => {
     //res.send('form'); escribe 'Form' en el html.
     res.render('links/add');
-}) 
+});
+
+router.post('/add', (req,res) => {
+    res.send('received');
+})
 
 module.exports = router;
