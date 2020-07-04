b2e46b257517e57a29bd1a9ea9f66acfe45804e6
FabianCruz-0 <fabiancruz3773@gmail.com>
2020-07-03 22:08:29 -0500

    signup view initialization

--- src/routes/autentication.js
+++ src/routes/autentication.js
@@ -2,4 +2,13 @@
 const express = require('express');
 const router = express.Router();
 
+//manejador de petición.
+router.get('/signup', (req,res) => {
+res.render('auth/signup');
+});
+
+router.post('/signup', (req,res) => {
+
+});
+
 module.exports = router;
