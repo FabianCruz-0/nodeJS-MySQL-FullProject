0bcf62f6404828be819622dbd805557911ef5e6d
FabianCruz-0 <fabiancruz3773@gmail.com>
2020-07-07 23:08:54 -0500

    forma general de matching passwords

---
+++ src/lib/helpers.js
@@ -0,0 +1,23 @@
+// funciones para ayuda.
+const bcrypt = require('bcryptjs');
+//bcryptjs
+const helpers = { };
+
+helpers.encryptPass = async (password) => {
+const salt = await bcrypt.genSalt(10); //funcion que genera un hash. recibe de parametro el numero de ejecuciones (más, más seguro pero lento);
+
+//metodo que cifra la contraseña.
+const passCrypted = await bcrypt.hash(password,salt);
+return passCrypted;
+};
+
+helpers.matchPass = async (password,savedPassword) => {
+try{
+    await bcrypt.compare(password,savedPassword);
+}catch(e){
+    console.log(e); //también se puede usar req.flash.
+}
+}
+
+
+module.exports = helpers;
