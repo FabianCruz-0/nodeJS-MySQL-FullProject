77ddde2e9f8ff4080c66e797b06acdf882e92637
FabianCruz-0 <fabiancruz3773@gmail.com>
2020-07-08 00:08:20 -0500

    routes singin added

--- src/routes/autentication.js
+++ src/routes/autentication.js
@@ -28,6 +28,19 @@ router.post('/signup', passport.authenticate('local.signup', {
     failureFlash: true
 }));
 
+router.get('/signin', (req,res) => {
+res.render('auth/signin');
+});
+
+router.post('/signin', (req,res,next) => {
+passport.authenticate('local.signin', {
+    successRedirect: '/profile',
+    failureRedirect: '/signin',
+    failureFlash:true
+})(req,res,next)
+});
+
+
 router.get('/profile', (req,res) => {
     res.send('profile');
 })
